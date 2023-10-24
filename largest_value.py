7142e6983f22934c48d18d898e04fd719e249580
Maurício Antunes <mauricio.abreua@gmail.com>
2023-10-24 19:58:41 -0300

    Implement largest values

--- largest_value.py
+++ largest_value.py
@@ -48,7 +48,36 @@ def bfs(root):
     return values
 
 
+def largest_values(root):
+    if not root:
+        return []
+
+    queue = deque([root])
+    largest = []
+
+    while queue:
+        size = len(queue)
+        max_val = float("-inf")
+
+        for i in range(size):
+            curr_node = queue.popleft()
+
+            if curr_node.val > max_val:
+                max_val = curr_node.val
+
+            if curr_node.left:
+                queue.append(curr_node.left)
+
+            if curr_node.right:
+                queue.append(curr_node.right)
+
+        largest.append(max_val)
+
+    return largest
+
+
 if __name__ == '__main__':
     root = Node(1, Node(3, Node(5), Node(3)), Node(2, None, Node(9)))
     print(dfs(root)) # [1, 3, 5, 3, 2, 9]
     print(bfs(root)) # [1, 3, 2, 5, 3, 9]
+    print(largest_values(root)) # [1, 3, 9]
